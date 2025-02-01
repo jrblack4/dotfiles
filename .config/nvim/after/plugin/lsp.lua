0628900bfe9325f4f202a3ed99ceb8fc130fd471
joel <jrblack4@protonmail.com>
2025-01-31 19:53:40 -0600

    snacks and settings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,17 +1,46 @@
 local lspconfig = require("lspconfig")
 
+-- Common options for all language servers.
+local common_opts = {
+  on_attach = function(_, bufnr)
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+  end,
+}
+
+-- Additional options specifically for lua_ls.
+local lua_opts = {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },  -- Recognize the `vim` global
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+    },
+  },
+}
+
+-- Setup handlers for all servers.
 require("mason-lspconfig").setup_handlers({
   function(server_name)
-    lspconfig[server_name].setup {
-      on_attach = function(_, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
-      end
-    }
+    -- Start with the common options.
+    local opts = common_opts
+
+    -- If the server is lua_ls, merge in the lua-specific settings.
+    if server_name == "lua_ls" then
+      opts = vim.tbl_deep_extend("force", {}, common_opts, lua_opts)
+    end
+
+    -- Setup the server with the options.
+    lspconfig[server_name].setup(opts)
   end,
 })
+
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -45,3 +45,9 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
 	end,
 })
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false, border = "rounded" })
+  end,
+})
